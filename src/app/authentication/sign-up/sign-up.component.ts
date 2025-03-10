45f41b066f9beb9e51ae2b8647113b09af41f21c
Gerth123 <robingerth21@gmail.com>
2025-03-10 21:49:16 +0100

    Added the sign up form.

--- src/app/authentication/sign-up/sign-up.component.ts
+++ src/app/authentication/sign-up/sign-up.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RoutingService } from '../../shared/services/routing-service/routing.service';
+import { ToastService } from '../../shared/services/toast-service/toast.service';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.scss'
 })
 export class SignUpComponent {
+  signUpForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private routingService: RoutingService,
+    private toastService: ToastService
+  ) {
+    this.signUpForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      rememberMe: [false]
+    });
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      this.toastService.show('Sign up successful!' + this.signUpForm.value.email + this.signUpForm.value.password);
+      setTimeout(() => this.routingService.navigateTo('/log-in'), 20000);
+    }
+  }
 }
