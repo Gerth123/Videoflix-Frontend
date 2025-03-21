2774ce9e1f0321b08c12c81502df3ab7415203b1
Gerth123 <robingerth21@gmail.com>
2025-03-21 22:10:21 +0100

    Added the desgin for the forgot-password component.

--- src/app/authentication/forgot-password/forgot-password.component.ts
+++ src/app/authentication/forgot-password/forgot-password.component.ts
@@ -1,13 +1,36 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../shared/header/header.component';
 import { FooterComponent } from '../../shared/footer/footer.component';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { RoutingService } from '../../shared/services/routing-service/routing.service';
+import { ToastService } from '../../shared/services/toast-service/toast.service';
+import { ApiService } from '../../shared/services/api-service/api.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-forgot-password',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [HeaderComponent, FooterComponent, ReactiveFormsModule, NgClass],
   templateUrl: './forgot-password.component.html',
-  styleUrl: './forgot-password.component.scss'
+  styleUrl: './forgot-password.component.scss',
 })
 export class ForgotPasswordComponent {
+  resetPasswordForm: FormGroup;
 
+  constructor(
+    private fb: FormBuilder,
+    private routingService: RoutingService,
+    private toastService: ToastService,
+    private apiService: ApiService
+  ) {
+    this.resetPasswordForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+
+  onSubmit() {}
 }
